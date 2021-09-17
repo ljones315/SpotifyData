da5de6785564f519ad2002d4af8e9be590166dc0
Jones, Liam Thomas <ljones315@gatech.edu>
2021-09-17 14:20:50 -0400

    Renamed top_tracks to server, added artist data

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const path = require('path');
 const spotifyApi = new SpotifyWebApi();
 spotifyApi.setAccessToken(process.env.ACCESS_TOKEN);
 const tracks = [];
+const artists = [];
 
 // spotifyApi.getMyTopTracks({ limit: 50, time_range: 'medium_term' })
 //   .then(function(data) {
@@ -23,7 +24,7 @@ const port = 3000;
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
-  res.render('index', {tracks : tracks});
+  res.render('index', {tracks : tracks, artists: artists});
 });
 
 app.use('/public', express.static('public'));
@@ -43,6 +44,17 @@ spotifyApi.getMyTopTracks({ limit: 50, time_range: 'short_term' })
   }, function(err) {
     console.log('Something went wrong!', err);
   });
+
+  spotifyApi.getMyTopArtists({ limit: 50, time_range: 'short_term'})
+  .then(function(data) {
+    let topArtists = data.body.items;
+    topArtists.forEach(function(artist, index) {
+      artists.push({index : index + 1, name : artist.name});
+    })
+  }, function(err) {
+    console.log('Something went wrong!', err);
+  });
+
 
   
 
