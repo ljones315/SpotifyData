87389c87676545db95dc1c2cf18903936d821262
Jones, Liam Thomas <ljones315@gatech.edu>
2021-09-16 22:11:12 -0400

    spruces up the page and adds artists for each track

--- top_tracks.js
+++ top_tracks.js
@@ -34,11 +34,11 @@ app.use('/public', express.static('public'));
 
 
 
-spotifyApi.getMyTopTracks({ limit: 50, time_range: 'medium_term' })
+spotifyApi.getMyTopTracks({ limit: 50, time_range: 'short_term' })
   .then(function(data) {
     let topTracks = data.body.items;
     topTracks.forEach(function(track, index) {
-      tracks.push({index : index + 1, name : track.name});
+      tracks.push({index : index + 1, name : track.name, artist: track.artists[0].name});
     })
   }, function(err) {
     console.log('Something went wrong!', err);
